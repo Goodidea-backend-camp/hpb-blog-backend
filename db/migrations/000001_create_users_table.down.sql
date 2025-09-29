b04b783906f211eacf0ca243c2fed6ba45f0bdea
Way <k8652090202@gmail.com>
2025-09-29 08:55:53 +0800

    feat: migrate posts table

--- db/migrations/000001_create_users_table.down.sql
+++
@@ -1,3 +0,0 @@
--- 本 Migration 僅為測試用途，實際使用時請依照資料庫結構調整，勿直接套用
-
-DROP TABLE IF EXISTS users;
--- db/migrations/000001_create_users_table.up.sql
+++
@@ -1,8 +0,0 @@
--- 本 Migration 僅為測試用途，實際使用時請依照資料庫結構調整，勿直接套用
-
-CREATE TABLE IF NOT EXISTS users(
-   user_id serial PRIMARY KEY,
-   username VARCHAR (50) UNIQUE NOT NULL,
-   hashed_password VARCHAR (60) NOT NULL,
-   email VARCHAR (300) UNIQUE NOT NULL
-);
