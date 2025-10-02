dfe9176fbb9cba37e9f56b119b7205ed2eead128
Way <k8652090202@gmail.com>
2025-10-02 14:35:43 +0800

    fix: increase username and password field sizes in users table

--- db/migrations/20250928175128_create_users_table.up.sql
+++ db/migrations/20250928175128_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(60) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT now(),
     updated_at TIMESTAMPTZ DEFAULT now(),
     deleted_at TIMESTAMPTZ
