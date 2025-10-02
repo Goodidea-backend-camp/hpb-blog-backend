2ef4199267204527c57474e4db59f8e1894bcbb7
Way <k8652090202@gmail.com>
2025-10-02 18:52:27 +0800

    [HPB-155] [BE] 根據資料庫文件，建立 User Database Migration (#7)
    * feat: migrate users table
    
    * fix: increase username and password field sizes in users table

---
+++ db/migrations/20250928175128_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT now(),
+    updated_at TIMESTAMPTZ DEFAULT now(),
+    deleted_at TIMESTAMPTZ
+);
